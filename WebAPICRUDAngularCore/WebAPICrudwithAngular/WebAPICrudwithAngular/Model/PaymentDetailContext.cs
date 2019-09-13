d452895202013c4bc72a91dd8154d9ad5d65f972
ozhangulal <ozhangulal@outlook.com>
2019-09-13 15:50:09 +0300

    DotNetCoreWebAPIwithAngular7CRUD
    DotNetCore WebAPI with Angular7 CRUD you can find all basic operations....

---
+++ WebAPICRUDAngularCore/WebAPICrudwithAngular/WebAPICrudwithAngular/Model/PaymentDetailContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPICrudwithAngular.Model
+{
+    public class PaymentDetailContext:DbContext
+    {
+        public PaymentDetailContext(DbContextOptions<PaymentDetailContext> options):base(options)
+        {
+
+        }
+        public DbSet<PaymentDetail> PaymentDetails { get; set; }
+    }
+}
---
+++ WebAPICRUDAngularCore/WebAPICrudwithAngular/WebAPICrudwithAngular/Startup.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json.Serialization;
+using WebAPICrudwithAngular.Model;
+
+namespace WebAPICrudwithAngular
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2); -> normally method like that but we want to change because of data coming lower letter as different then database also we want how its look like in database we want to see like that in the application so we are gonna change method.
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2).AddJsonOptions(options =>
+            {
+                var resolver = options.SerializerSettings.ContractResolver;
+                if (resolver != null)
+                    (resolver as DefaultContractResolver).NamingStrategy = null;
+            });
+
+            services.AddDbContext<PaymentDetailContext>(options=>options.UseSqlServer(Configuration.GetConnectionString("DevConnection")));
+
+            services.AddCors();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseCors(options =>
+            options.WithOrigins("http://localHost:4200").AllowAnyMethod().AllowAnyHeader());
+
+            app.UseMvc();
+        }
+    }
+}
